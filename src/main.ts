ca5cc014ae10cf89c4f7b82dc25ddf68794c8ef4
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2024-05-18 15:51:00 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = express();
+
 export async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  server.get('/', (req, res) => res.json({ message: 'hello word' }));
   app.use(cors());
   await app.listen(process.env.PORT, () =>
     console.log(`API online on port ${process.env.PORT}`),
