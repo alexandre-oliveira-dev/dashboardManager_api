c4a1664fdbc6c8819ab100d88117de58c2adfa58
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2024-05-18 13:59:23 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
+
   await app.listen(5000, () => console.log(`API online on port 5000`));
 }
 bootstrap();
